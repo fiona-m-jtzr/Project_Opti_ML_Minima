0c4b57a44fcb0240fa98cb99832be175713274a7
MathiasMacDougall <mathias.macdougall@epfl.ch>
2026-06-07 18:24:32 +0200

    log10 sur loss

--- notebooks/loss_around_min_a.py
+++ notebooks/loss_around_min_a.py
@@ -107,7 +107,7 @@ for i, x_coeff in enumerate(steps_x):
         
         # Calculer la loss à cette coordonnée
         loss_val = evaluate_loss(model, testloader, criterion)
-        Z[j, i] = loss_val # Attention à l'indexation (y=lignes, x=colonnes)
+        Z[j, i] = np.log10(loss_val) # Attention à l'indexation (y=lignes, x=colonnes)
         
     print(f"Ligne {i+1}/{grid_resolution} terminée.")
 
