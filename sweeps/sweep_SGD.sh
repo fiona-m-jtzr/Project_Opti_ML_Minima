9dc8538d335627f8414197f108e937261275afeb
fiona-m-jtzr <fiona.jetzer@epfl.ch>
2026-06-12 17:38:32 +0200

    Corrected sweep_SGD.sh

--- sweeps/sweep_SGD.sh
+++ sweeps/sweep_SGD.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for lr in 0.05 0.1; do
+for lr in 0.01 0.05 0.1; do
     for bs in 64, 128, 256; do
-            python /mloscratch/homes/jetzer/Project_Opti_ML_Minima/RUN.py \
+                python RUN.py \
                 --model resnet20 \
                 --optimizer sgd \
                 --lr $lr \
@@ -17,15 +17,9 @@ for lr in 0.05 0.1; do
 done
 
 
-for lr in 0.01; do
+for lr in 0.01 0.05 0.1; do
     for bs in 64 128 256; do
-        lr_tag=$(echo $lr | tr '.' 'p')
-        python csub.py \
-            -n "train-sgd-resnet-lr${lr_tag}-bs${bs}" \
-            -g 1 \
-            --train \
-            --venv /mloscratch/homes/jetzer/Project_Opti_ML_Minima/.venv \
-            --command "python /mloscratch/homes/jetzer/Project_Opti_ML_Minima/RUN.py \
+                python RUN.py \
                 --model resnet20 \
                 --optimizer sgd \
                 --lr $lr \
@@ -35,6 +29,6 @@ for lr in 0.01; do
                 --weight_decay 0.0 \
                 --epochs 600 \
                 --scheduler cosine \
-                --seed 1"
+                --seed 1
     done
 done
