ebabc2dead171c258460b7bced4f480d8c8d611c
fiona-m-jtzr <fiona.jetzer@epfl.ch>
2026-06-12 16:08:51 +0200

    Corrected sweep_SGD

--- sweeps/sweep_SGD.sh
+++ sweeps/sweep_SGD.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-for lr in 0.01 0.05 0.1; do
-    for bs in 64 128 256; do
-        lr_tag=$(echo $lr | tr '.' 'p')
-        python csub.py \
-            -n "train-sgd-vit-lr${lr_tag}-bs${bs}" \
-            -g 1 \
-            --train \
-            --venv /mloscratch/homes/jetzer/Project_Opti_ML_Minima/.venv \
-            --command "python /mloscratch/homes/jetzer/Project_Opti_ML_Minima/RUN.py \
-                --model vit \
+for lr in 0.05 0.1; do
+    for bs in 64, 128, 256; do
+            python /mloscratch/homes/jetzer/Project_Opti_ML_Minima/RUN.py \
+                --model resnet20 \
                 --optimizer sgd \
                 --lr $lr \
                 --batch_size $bs \
@@ -17,13 +11,13 @@ for lr in 0.01 0.05 0.1; do
                 --nesterov \
                 --weight_decay 0.0 \
                 --epochs 600 \
-                --scheduler cosine_warmup \
-                --warmup_epochs 15 \
-                --seed 1"
+                --scheduler cosine \
+                --seed 1
     done
 done
 
-for lr in 0.01 0.05 0.1; do
+
+for lr in 0.01; do
     for bs in 64 128 256; do
         lr_tag=$(echo $lr | tr '.' 'p')
         python csub.py \
@@ -39,7 +33,7 @@ for lr in 0.01 0.05 0.1; do
                 --momentum 0.9 \
                 --nesterov \
                 --weight_decay 0.0 \
-                --epochs 400 \
+                --epochs 600 \
                 --scheduler cosine \
                 --seed 1"
     done
